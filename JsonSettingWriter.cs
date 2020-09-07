0fdc2a87dbcd73f6f35b5f828e1d2898b445e34a
satrex <info@satrex.jp>
2020-09-07 16:18:37 +0900

    added functionality of automatic appsettings.json placement (when file doesn't exist)

--- JsonSettingWriter.cs
+++ JsonSettingWriter.cs
@@ -3,14 +3,28 @@ using Newtonsoft;
 
 namespace Lazorm
 {
+    /// <summary>
+    /// Write connectionstring entry into appsettings.json
+    /// </summary>
     public static class JsonSettingWriter
     {
 
+        /// <summary>
+        /// Write connectionstring entry into appsettings.json
+        /// </summary>
+        /// <param name="key">entry key name</param>
+        /// <param name="value">entry value connectionstring</param>
+        /// <param name="appSettingsJsonFilePath">(optional) appsetting.json file path</param>
         public static void SetAppSettingValue(string key, string value, string appSettingsJsonFilePath = null)
         {
             if (appSettingsJsonFilePath == null)
             {
                 appSettingsJsonFilePath = System.IO.Path.Combine(System.Environment.CurrentDirectory, "appsettings.json");
+            }
+            if (!System.IO.File.Exists(appSettingsJsonFilePath))
+            {
+                var source = System.IO.Path.Combine(System.AppContext.BaseDirectory, "appsettings.json.template");
+                System.IO.File.Copy(source, appSettingsJsonFilePath);
             }
 
             var json = System.IO.File.ReadAllText(appSettingsJsonFilePath);
