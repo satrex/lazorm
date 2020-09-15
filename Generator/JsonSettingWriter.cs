82409d605062b9f1c4c3ee6e3d46201e36b1f398
satrex <info@satrex.jp>
2020-09-16 02:08:08 +0900

    fixed error with json writing

--- Generator/JsonSettingWriter.cs
+++ Generator/JsonSettingWriter.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Newtonsoft;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Lazorm
 {
@@ -17,6 +19,8 @@ namespace Lazorm
         /// <param name="appSettingsJsonFilePath">(optional) appsetting.json file path</param>
         public static void SetAppSettingValue(string key, string value, string appSettingsJsonFilePath = null)
         {
+            Console.WriteLine("\n{0} started.", System.Reflection.MethodInfo.GetCurrentMethod().Name);
+
             if (appSettingsJsonFilePath == null)
             {
                 appSettingsJsonFilePath = System.IO.Path.Combine(System.Environment.CurrentDirectory, "appsettings.json");
@@ -28,14 +32,29 @@ namespace Lazorm
             }
 
             var json = System.IO.File.ReadAllText(appSettingsJsonFilePath);
-            dynamic jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject<Newtonsoft.Json.Linq.JObject>(json);
+            // dynamic jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject<Newtonsoft.Json.Linq.JObject>(json);
+            JToken token =  JObject.Parse(json);
+            dynamic jsonObj = JsonConvert.DeserializeObject<dynamic>(token.ToString());
+            const string CONSTR = "ConnectionStrings";
 
-            jsonObj["ConnectionStrings"][key] = value;
-
+            if(!jsonObj.ContainsKey(CONSTR))
+            {
+                Console.WriteLine("{0} node does not exist", CONSTR);
+                jsonObj.Add(CONSTR, new JObject());
+                System.Threading.Thread.Sleep(200);
+                jsonObj[CONSTR].Add(key, value);
+                Console.WriteLine("{0} node added.", CONSTR);
+            }
+            else
+            {
+                Console.WriteLine("{0} node exists", CONSTR);
+                jsonObj[CONSTR][key] = value;
+            }
 
             string output = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObj, Newtonsoft.Json.Formatting.Indented);
 
             System.IO.File.WriteAllText(appSettingsJsonFilePath, output);
+            Console.WriteLine("connection is written in config file {0} ", appSettingsJsonFilePath);
         }
 
     }
